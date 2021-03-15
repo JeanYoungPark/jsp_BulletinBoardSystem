002abe44596023afa31cced84841c953be957924
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2021-03-15 20:14:03 +0900

    class 수정

--- src/bbs/BbsDAOTmp.java
+++ src/bbs/BbsDAOTmp.java
@@ -12,9 +12,11 @@ public class BbsDAOTmp {
 	
 	public BbsDAOTmp() {
 		try {
-			String dbURL = "jdbc:mysql://localhost:3306/BBS";
-			String dbID = "root";
-			String dbPassword="";
+			 
+			 String dbURL = "jdbc:mysql://localhost:3306/BBS";
+			 String dbID = "root";
+			 String dbPassword="";
+			 
 			Class.forName("com.mysql.jdbc.Driver");
 			conn = DriverManager.getConnection(dbURL,dbID,dbPassword);
 		} catch (Exception e) {
@@ -37,7 +39,7 @@ public class BbsDAOTmp {
 	}
 	
 	public int getNext() {
-		String SQL = "SELECT bbsID FROM BBS ORDER BY bbsID DESC";
+		String SQL = "SELECT bbsID FROM bbs ORDER BY bbsID DESC";
 		try {
 			PreparedStatement pstmt = conn.prepareStatement(SQL);
 			rs=pstmt.executeQuery();
@@ -52,7 +54,7 @@ public class BbsDAOTmp {
 	}
 	
 	public int write(String bbsTitle, String userID, String bbsContent) {
-		String SQL = "INSERT INTO BBS VALUES (?, ?, ?, ?, ?, ?)";
+		String SQL = "INSERT INTO bbs VALUES (?, ?, ?, ?, ?, ?, ?)";
 		try {
 			PreparedStatement pstmt = conn.prepareStatement(SQL);
 			pstmt.setInt(1, getNext());
@@ -61,6 +63,7 @@ public class BbsDAOTmp {
 			pstmt.setString(4, getDate());
 			pstmt.setString(5, bbsContent);
 			pstmt.setInt(6, 1);
+			pstmt.setInt(7, 0);
 			return pstmt.executeUpdate();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -69,7 +72,7 @@ public class BbsDAOTmp {
 	}
 	
 	public ArrayList<Bbs> getList (int pageNumber){
-		String SQL = "SELECT * FROM BBS WHERE bbsID < ? AND bbsAvailable = 1 ORDER BY bbsID DESC LIMIT 10";
+		String SQL = "SELECT * FROM bbs WHERE bbsID < ? AND bbsAvailable = 1 ORDER BY bbsID DESC LIMIT 10";
 		ArrayList<Bbs> list = new ArrayList<Bbs>();
 		try {
 			PreparedStatement pstmt = conn.prepareStatement(SQL);
@@ -92,7 +95,7 @@ public class BbsDAOTmp {
 	}
 	
 	public boolean nextPage(int pageNumber) {
-		String SQL = "SELECT * FROM BBS WHERE bbsID < ? AND bbsAvailable = 1 ORDER BY bbsID DESC LIMIT 10";
+		String SQL = "SELECT * FROM bbs WHERE bbsID < ? AND bbsAvailable = 1 ORDER BY bbsID DESC LIMIT 10";
 		try {
 			PreparedStatement pstmt = conn.prepareStatement(SQL);
 			pstmt.setInt(1, getNext() - (pageNumber -1) * 10);
@@ -107,7 +110,7 @@ public class BbsDAOTmp {
 	}
 	
 	public Bbs getBbs(int bbsID) {
-		String SQL = "SELECT * FROM BBS WHERE bbsID = ?";
+		String SQL = "SELECT * FROM bbs WHERE bbsID = ?";
 		try {
 			PreparedStatement pstmt = conn.prepareStatement(SQL);
 			pstmt.setInt(1, bbsID);
@@ -129,7 +132,7 @@ public class BbsDAOTmp {
 	}
 	
 	public int update(int bbsID, String bbsTitle, String bbsContent) {
-		String SQL = "UPDATE BBS SET bbsTitle = ?, bbsContent = ? WHERE bbsID=?";
+		String SQL = "UPDATE bbs SET bbsTitle = ?, bbsContent = ? WHERE bbsID=?";
 		try {
 			PreparedStatement pstmt = conn.prepareStatement(SQL);
 			pstmt.setString(1, bbsTitle);
@@ -143,7 +146,7 @@ public class BbsDAOTmp {
 	}
 	
 	public int delete(int bbsID) {
-		String SQL = "UPDATE BBS SET bbsAvailable=0 WHERE bbsID=?";
+		String SQL = "UPDATE bbs SET bbsAvailable=0 WHERE bbsID=?";
 		try {
 			PreparedStatement pstmt = conn.prepareStatement(SQL);
 			pstmt.setInt(1, bbsID);
@@ -182,5 +185,24 @@ public class BbsDAOTmp {
 			e.printStackTrace();
 		}
 		return list;
+	}
+	
+	public int boardNum() {
+		String SQL = "SELECT * FROM bbs";
+		int result = 0;
+		
+		//레코드 수 불러오기
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			rs=pstmt.executeQuery();
+			rs.last();
+			int resultTmp = rs.getRow()/10; 
+			result = (rs.getRow()%5 == 0)?resultTmp:resultTmp+1;
+			rs.beforeFirst();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return result;
 	}
 }
